f7f39b99d89f681fb412ac67b70dc9be73aac2b6
Mukul <raomukulkumar11@gmail.com>
2026-08-18 18:50:57 +0530

    Updated project

--- DAILY_CLASSWORK/app.js
+++ DAILY_CLASSWORK/app.js
@@ -62,38 +62,38 @@
 
 
 
-// const students = [
-//     { id: 1, name: 'Gyan', age: 23 },
-//     { id: 2, name: 'Gyan', age: 23 },
-//     { id: 2, name: 'Gyan', age: 23 },
-//     { id: 2, name: 'Gyan', age: 23 }
-// ];
-// const http = require('http');
-// require("dotenv").config();
-// const process = require('process');
-// const port = process.env.PORT || 3000;
-// const server = http.createServer((req, res) => {
-//      if (req.method === 'GET' && req.url === '/api/students') {
+const students = [
+    { id: 1, name: 'Gyan', age: 23 },
+    { id: 2, name: 'Gyan', age: 23 },
+    { id: 2, name: 'Gyan', age: 23 },
+    { id: 2, name: 'Gyan', age: 23 }
+];
+const http = require('http');
+require("dotenv").config();
+const process = require('process');
+const port = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+     if (req.method === 'GET' && req.url === '/api/students') {
 
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.writeHead(200, { 'Content-Type': 'text/html' });
 
-//         res.end(
-//             JSON.stringify({count: students.length, students})
-//         ); 
-//     } 
-//     else if(req.method === 'GET' && req.url === '/api/students/count'){
-//     res.writeHead(200, { 'Content-Type': 'application/json' });
-//     res.end(JSON.stringify({count: students.length}));
-//    }
-//    else{
-//     res.writeHead(404, { 'Content-Type': 'application/json' });
-//     res.end(JSON.stringify({count: students.length}));
-//    }
+        res.end(
+            JSON.stringify({count: students.length, students})
+        ); 
+    } 
+    else if(req.method === 'GET' && req.url === '/api/students/count'){
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({count: students.length}));
+   }
+   else{
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({count: students.length}));
+   }
 
-// });
-// server.listen(port, () => {
-//     console.log(`Server is running on port ${port}`);
-// });  
+});
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});  
       
 
     
